48f2e35e1dcd49ddd2230bf254e2b7fb1b9b2828
nikita <eno004731@gmail.com>
2026-05-07 13:19:47 +0300

    add SecurityFilterChain

---
+++ bd/demo/src/main/java/my_jira/users/SecurityConfig.java
@@ -0,0 +1,48 @@
+package my_jira.users;
+
+// Говорим Spring, что это конфигурационный класс
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// Интерфейс encoder-а, который будем внедрять в сервис
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+// Реализация BCrypt для паролей
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+
+import static jakarta.servlet.DispatcherType.ERROR;
+import static jakarta.servlet.DispatcherType.FORWARD;
+
+@Configuration
+public class SecurityConfig {
+
+    // Spring вызовет этот метод сам и зарегистрирует PasswordEncoder как bean
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // Возвращаем реализацию BCrypt
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public  SecurityFilterChain web(HttpSecurity http){
+        http
+        
+        .authorizeHttpRequests((authorize) -> authorize
+            .dispatcherTypeMatchers(FORWARD, ERROR).permitAll()
+            .requestMatchers("/api/auth/register").permitAll()
+            .requestMatchers("/api/auth/login").permitAll()
+            
+            .anyRequest().authenticated())
+            .httpBasic(AbstractHttpConfigurer::disable)
+
+            
+            .formLogin(AbstractHttpConfigurer::disable);
+            
+        
+
+   
+    return http.build();
+    }
+}
