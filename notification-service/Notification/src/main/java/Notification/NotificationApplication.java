63e0f6253b7d9d4f61c68ac2ac30a75c463dd0fa
nikita <eno004731@gmail.com>
2026-06-03 08:38:54 +0300

    fix

--- notification-service/Notification/src/main/java/Notification/NotificationApplication.java
+++ notification-service/Notification/src/main/java/Notification/NotificationApplication.java
@@ -2,9 +2,11 @@ package Notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableKafka
 @EnableScheduling
 public class NotificationApplication {
 
